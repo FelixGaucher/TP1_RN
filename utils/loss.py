06ec54d5071b4425db683bbbd0eb4fd3ee3da007
FelixGaucher <felix.gaucher@laposte.net>
2021-01-25 15:58:50 +0100

    implements vectorized loss for softmax

--- utils/loss.py
+++ utils/loss.py
@@ -38,23 +38,32 @@ def softmax_ce_naive_forward_backward(X, W, y, reg):
     print("y shape : ", y.shape)
     
     for i in range(N):
-        #dot product
-        y_w = np.dot(X[i,:],W)
         
+        #dot product
+        x = np.array([X[i]])
+        
+        y_w = np.dot(x, W)
         #calcul du softmax + normalisation
         y_exp = np.exp(y_w)
         S = y_exp / np.sum(y_exp)
-        
         #calcul de la loss
-        loss_tmp = -np.log(S[y[i]]) + 0.5*reg*(np.linalg.norm(X[i])**2)
+        loss_tmp = -np.log(S[0, y[i]])
         loss += loss_tmp
         
         #calcul du gradient pour une donnée
         t = np.zeros((1, C))
+        t = t[0]
         t[y[i]] = 1
-        grad_tmp = np.dot((S - t), X[i]).T + 2*reg*W
+        
+        S_t = S - t
+        S_t = np.array(S_t).T
+        grad_tmp = np.dot(S_t, x).T + 2*reg*W
+        dW += grad_tmp
 
     loss /= N
+    dW /= N
+    
+    loss += 0.5*reg*(np.linalg.norm(W)**2)
 
     return loss, dW
 
@@ -85,6 +94,23 @@ def softmax_ce_forward_backward(X, W, y, reg):
     dW = np.zeros(W.shape)
     ### TODO ###
     # Ajouter code ici #
+    
+    #dot product
+    y_w = np.dot(X,W)
+    
+    #calcul du softmax vectorisé
+    y_w = np.exp(y_w)
+    y_sum = np.array([np.sum(y_w,1)])
+    y_sum = y_sum.T
+    S = y_w / y_sum
+   
+    #calcul de la cross-entropy loss vectorisé
+    predictions = S[np.arange(len(S)), y]
+    loss = -np.sum(np.log(predictions))
+    loss /= N
+    loss += 0.5*reg*(np.linalg.norm(W)**2)
+    
+    #calcul du gradient vectorisé
     
     
     return loss, dW
